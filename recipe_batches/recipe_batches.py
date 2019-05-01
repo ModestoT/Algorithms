4c9b96433c644c37677521d136d4cda2b87260f6
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-01 16:40:51 -0500

    Completed the recipe file and passed all test

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,37 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # Get how many batches can be made with the available ingredients compared to the recipes needed ingredients
+  # Compare the dictonaries of the recipe to the ingredients 
+  # if the ingredients dic does not have one of the keys or not enough of one ingredient from the recipe return 0
+  # else get the number of times the recipe can be made
+  batches = 0
+  current_difference = None
+
+  for i, k in recipe.items():
+    if i in ingredients:
+      if k <= ingredients[i]:
+        difference = ingredients[i] // k
+        print('first if',difference)
+  
+        if current_difference:
+          if current_difference > difference:
+            print('second if', difference)
+            current_difference = difference
+        else:
+          print('else1',difference)
+          current_difference = difference
+      else:
+        return 0
+    else:
+      return 0
+  return current_difference
+   
 
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
   # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  recipe = { 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }
+  ingredients = { 'milk': 1288, 'flour': 9, 'sugar': 95 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
