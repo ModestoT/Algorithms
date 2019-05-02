d41706bc6a7479088f888764eebdbab9524062fc
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-02 18:52:20 -0500

    completed making_change problem

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,9 +3,21 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
-
-
+  res = [0] * amount
+  if amount == 0:
+    print(amount, denominations)
+    return 1
+  elif amount < 0 or denominations == []:
+    return 0
+  else:
+    # for coin in denominations:
+    #   print('coin',coin)
+    #   for higher_amount in range(coin, amount+1):
+    #     print('amount',higher_amount)
+    #     res[higher_amount-1] = higher_amount - coin
+    # return res
+    # return  making_change((amount- 50), denominations) + making_change((amount- 25), denominations) + making_change((amount- 10), denominations) + making_change((amount- 5), denominations) + making_change((amount- 1), denominations) 
+    return  making_change(amount, denominations[:-1]) + making_change((amount - denominations[-1]), denominations) 
 if __name__ == "__main__":
   # Test our your implementation from the command line
   # with `python making_change.py [amount]` with different amounts
