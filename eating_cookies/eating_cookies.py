4f3350c024cc8a2302183dd4bf052b4c9439a17c
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-02 13:32:34 -0500

    Refactored code so that it can do the large numbers tests

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -5,7 +5,7 @@ import sys
 # The cache parameter is here for if you want to implement
 # a solution that is more efficient than the naive 
 # recursive solution
-def eating_cookies(n, cache=None):
+def eating_cookies(n, cache = None):
   # Take n and go all the way down to 1
   # Check how many times n - 1 goes into the starting n
   # add up all the counts 
@@ -15,8 +15,14 @@ def eating_cookies(n, cache=None):
     return 1
   elif n == 2:
     return 2
+  elif not cache:
+    cache = [0 for i in range(n+1)]
+    return eating_cookies(n, cache)
+  elif cache[n] > 0:
+    return cache[n]
   else:
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+    return cache[n]
       
 
 if __name__ == "__main__":
