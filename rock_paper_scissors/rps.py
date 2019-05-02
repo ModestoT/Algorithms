ed0ee0d28ebfb441038e10eea6c2950dbc00de76
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-05-02 15:35:19 -0500

    completed the rps algo and passed the test file

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,22 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  # create a list that will hold all the possible cominations
+  # for however many combinations there are add the possible cominations to the list with out repeats
+  # create a starting list with the options of rock paper and scissors
+  # loop throug the length of the rps list and hold the starting option in a value
+  # do another loop that does a recusive call which gets the possible combinations from that starting value
+  rps = ['rock', 'paper', 'scissors']
+  res=[]
+  if n==0:
+      return [[]]
+  else:
+    for i in range(0,len(rps)):
+      start=rps[i]
+      for combo in rock_paper_scissors(n-1):
+          res.append([start]+combo)
 
+    return res
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
